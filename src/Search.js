2e96c1717c7c748899eddaccabe99a04b5e63aa2
mechaphysis <triptofante@gmail.com>
2018-08-07 17:37:18 +0200

    feat: Improve markup structure for Search

--- src/Search.js
+++ src/Search.js
@@ -3,7 +3,12 @@ import React, { Component } from 'react';
 class Search extends Component {
   render () {
     return (
-      <input type="text" role="search" placeholder="Filter points of interest by name"/>
+      <div className="search-poi">
+        <input type="text" role="search" placeholder="Filter points of interest by name"/>
+        <div className="search-poi-results">
+          <ol className="poi-list"></ol>
+        </div>
+      </div>
     );
   }
 }
