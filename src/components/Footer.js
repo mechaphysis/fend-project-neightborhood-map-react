3f8307e0dcf90818c5027d85010872e44c77dc53
Esperanza González <esperanza@mechaphysis.org>
2019-02-15 16:55:59 +0100

    refactor: move footer to its own component

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React, {Fragment} from 'react'
+
+function Footer() {
+    return (
+        <Fragment>
+            <footer className="footer">
+                    <p className="footer-details">
+                This Web-App was developed by <a href="https://github.com/mechaphysis">@mechaphysis</a> as the Capstone Project for Udacity Front End NanoDegree.
+                Information about the locations is provided by <a href="https://developer.foursquare.com/">FourSquare API</a>.
+            </p>
+            </footer>
+        </Fragment>
+    )
+}
+
+export default Footer
