faa03781e6eca5976314d559d81956280f529b3d
Esperanza González <esperanza@mechaphysis.org>
2019-02-26 10:03:04 +0100

    fix: adjust footer styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React, {Fragment} from 'react'
 function Footer() {
     return (
         <Fragment>
-            <footer className="footer container text-center pt-2">
+            <footer className="footer container text-right pt-2">
                     <small className="footer-details text-gray">
                 Developed by <a href="https://github.com/mechaphysis">@mechaphysis</a>. 
                 Information about the locations is provided by <a href="https://developer.foursquare.com/">FourSquare API</a>.
